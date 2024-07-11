bab7aa01896815f8d27095e814ddc8a06ca798fe
James Crovo <jamescrovo450@gmail.com>
2024-07-10 20:23:47 -0500

    fix ecosystem file again

--- client/ecosystem.config.js
+++ client/ecosystem.config.js
@@ -1,7 +1,10 @@
-export const apps = [{
-        name: 'buckshot',
-        script: 'build/index.js',
-        env: {
-            NODE_ENV: 'production'
-        }
-    }];
+export default {
+    apps: [{
+      name: 'buckshot',
+      script: 'build/index.js',
+      env: {
+        NODE_ENV: 'production',
+        PORT: 3010
+      }
+    }]
+  };
